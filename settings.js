f5ed29d5b3ff819e3efae16458c198689a516206
sakiya3 <107879859+sakiya3@users.noreply.github.com>
2022-06-25 16:48:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94769995891']
+global.premium = ['94769995891']
+global.ownernomer = '94769995891'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
